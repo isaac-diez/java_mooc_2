7f0ecb01351d830a0ff6fd4dfd37d55841c2c6ad
isaac.diez <isaac.diez@proton.me>
2024-12-14 17:51:53 +0100

    exercici 10_01, 10_02 i 10_03 finished

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,47 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        ArrayList<String> list = new ArrayList<>();
+
+        String input = "";
+        System.out.println("Input numbers, type \"end\" to stop.");
+
+        while (true) {
+
+            input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            list.add(input);
+
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+
+        String operation = scanner.nextLine();
+
+        if (operation.equals("p")) {
+            
+            double averageOfPositiveNumbers = list.stream()
+                    .mapToInt(s -> Integer.valueOf(s))
+                    .filter(number -> number > 0)
+                    .average()
+                    .getAsDouble();
+            
+            System.out.println("Average of the positive numbers: " + averageOfPositiveNumbers);
+
+        } else if (operation.equals("n")) {
+
+            double averageOfNegativeNumbers = list.stream()
+                    .mapToInt(s -> Integer.valueOf(s))
+                    .filter(number -> number < 0)
+                    .average()
+                    .getAsDouble();
+            
+            System.out.println("Average of the negative numbers: " + averageOfNegativeNumbers);
+        } 
 
     }
 }
--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -7,7 +8,42 @@ public class PositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
+        ArrayList<String> numbers = new ArrayList<>();
+
+        String input = "";
+
+        System.out.println("Input numbers or 00000 to stop");
+
+        while (true) {
+
+            input = scanner.nextLine();
+
+            if (input.equals("00000")) {
+                break;
+            }
+
+            numbers.add(input);
+
+        }
+
+        ArrayList<Integer> numbersReally = numbers.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        List<Integer> positiveNumbers = positive(numbersReally);
+
+        positiveNumbers.stream()
+                .forEach(value -> System.out.println(value));
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers) {
+
+        List<Integer> positives = numbers.stream()
+                .filter(number -> number > 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return positives;
 
     }
 
