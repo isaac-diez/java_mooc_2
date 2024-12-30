3d25673374cd243e17c16208f3ec13c4c5d799a5
isaac.diez <isaac.diez@proton.me>
2024-12-30 11:17:34 +0100

    Excercici 10_15 part 1 finished

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,18 @@
 
 
 public class Checker {
+    
+        public boolean isDayOfWeek(String string) {
+        
+        String regex = "mon|tue|wed|thu|fri|sat|sun";
+        
+        if (string.matches(regex)) {
+        
+            return true;
+            
+        } else return false;        
+        
+        
+    }
 
 }
